3b38585c7562e82b3c6e661cab8c8cdeb5b7e04e
obsq <63944348+obsq@users.noreply.github.com>
2020-04-20 21:03:41 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,8 +13,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`nthada ninikk vende?ntta muthalalikk jeevan undonn nokan vannathano?^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
-                     f"`My owner`: {DEFAULTUSER}\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @obsquriel\n"
-                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     "[Deploy this userbot Now](https://github.com/obsq/GujjuBot)")
+    await alive.edit("Hey,this is a message from my master @obsquriel..Oh shit..I should have never called him by name..forgive me master^.^\n\nMessage goes like this●●●\nIf any one want to talk with me first take approval from my bot.. if you get approval also I will be taking with you only if Iam free--by @obsquriel\n\n\nAll read the message?? Okay..there is no need to take approval to join my masters channel @crackedapps_obsq 【Remember that if channel attains 1k subs he will give bots like me free..】\nNever spam in my master's inbox... I will block you.. okay?Don't make my master angry\nFree approval for malayalees.. yeah keri va makkale\n\n`"
+                     f"`My owner`: {DEFAULTUSER}\n\n"
+                     "`Bot name:obsqofficialbot(jinn)\nfork by:` @obsquriel\n"
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`")
+                     
