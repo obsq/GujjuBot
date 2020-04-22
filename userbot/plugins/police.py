528baa45453ea4dbb3886a324cdc6f024b302594
obsq <63944348+obsq@users.noreply.github.com>
2020-04-22 10:01:29 +0530

    Update police.py

--- userbot/plugins/police.py
+++ userbot/plugins/police.py
@@ -8,7 +8,7 @@ from uniborg.util import admin_cmd
 from userbot import ALIVE_NAME
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "GujjuBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "obsq"
 
 @borg.on(admin_cmd(pattern=r"police"))
 
